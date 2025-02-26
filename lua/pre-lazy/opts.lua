629d49f2f23fdc87c0012a9bf80031cc3f7439ca
rokvisi <rokas.post@gmail.com>
2025-02-27 00:15:18 +0200

    replace deprecated vim.opt with vim.o

--- lua/pre-lazy/opts.lua
+++ lua/pre-lazy/opts.lua
@@ -6,56 +6,58 @@ vim.g.maplocalleader = " "
 vim.lsp.inlay_hint.enable(true)
 
 -- Fix windows context nvim shells when using powershell.
-vim.opt.shellcmdflag = '-c'
-vim.opt.shellxquote = ''
+vim.o.shellcmdflag = '-c'
+vim.o.shellxquote = ''
 
 -- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings are correct.
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- KASPARAS: We want to enable text wraps, because we have a cool and sneaky remap for j and k to comfortably move around wraps
-vim.opt.wrap = true
-vim.opt.breakindent = true
+vim.o.wrap = true
+vim.o.breakindent = true
 
 -- KASPRAS: better wrapping option
 vim.cmd("set whichwrap+=<,>,[,],h,l")
 
 -- Set line numbers (current line abosulte, others relative)
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.o.number = true
+vim.o.relativenumber = true
 
-vim.opt.virtualedit = "block" -- Allow cursor to move anywhere in visual block mode
-vim.opt.cursorline = true     -- Highlight current line
-vim.opt.termguicolors = true  -- Enable 24 bit color support
-vim.g.have_nerd_font = true   -- Enable Nerd-font support
-vim.opt.ignorecase = true     -- Ignore case when typing commands
+vim.o.virtualedit = "block" -- Allow cursor to move anywhere in visual block mode
+vim.o.cursorline = true     -- Highlight current line
+vim.o.termguicolors = true  -- Enable 24 bit color support
+vim.g.have_nerd_font = true -- Enable Nerd-font support
+vim.o.ignorecase = true     -- Ignore case when typing commands
 
-vim.opt.expandtab = true      -- expand tab input with spaces characters
-vim.opt.tabstop = 4           -- num of space characters per tab
-vim.opt.shiftwidth = 4        -- spaces per indentation level
-vim.opt.smartindent = true    -- syntax aware indentations for newline inserts
-vim.opt.expandtab = true      -- expand tab input with spaces characters
+vim.o.expandtab = true      -- expand tab input with spaces characters
+vim.o.tabstop = 4           -- num of space characters per tab
+vim.o.shiftwidth = 4        -- spaces per indentation level
+vim.o.smartindent = true    -- syntax aware indentations for newline inserts
+vim.o.expandtab = true      -- expand tab input with spaces characters
 
 vim.g.html_indent_script1 = "inc"
 vim.g.html_indent_style1 = "inc"
 vim.g.html_indent_inctags = "html,body,head,tbody"
 
 -- Split new windows below and to the right
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.o.splitbelow = true
+vim.o.splitright = true
 
 -- Disable the native status line.
-vim.opt.showmode = false -- disable text
-vim.opt.laststatus = 3   -- disable the line
+vim.o.showmode = false -- disable text
+vim.o.laststatus = 3   -- disable the line
 
--- Stop filling empty lines after the buffer with '~'.
-vim.opt.fillchars:append({ eob = '~' })
+-- Set fill characters for various elements.
+-- vim.opt.fillchars:append({ eob = ' ', fold = ' ', foldopen = '', foldsep = ' ', foldclose = '' })
+vim.opt.fillchars = { eob = ' ', fold = ' ', foldopen = '', foldsep = ' ', foldclose = '' }
 
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
 
 -- folds
-vim.o.foldenable = true -- Enable folding.
-vim.o.foldcolumn = '0'  -- Don't show fold column in the gutter.
-vim.o.foldlevel = 99    -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldmethod = "manual" -- needed for ufo
+vim.o.foldenable = true     -- Enable folding.
+vim.o.foldcolumn = '1'      -- Don't show fold column in the gutter.
+vim.o.foldlevel = 99        -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
